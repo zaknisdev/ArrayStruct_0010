cccb4a703ced5b8999d1b744315bbbb7879d06b6
zaknisdev <zakianisf1710@gmail.com>
2025-04-20 14:19:40 +0700

    membuat array dengan ukuran dan diberi nilai awal

--- array.cpp
+++ array.cpp
@@ -3,3 +3,7 @@ using namespace std;
 
 // deklarasi array tanpa ukuran, wajib diisi datanya
 int nilai[] = {1, 2, 4, 7};
+
+// deklarasi array dengan ukuran dan diberi nilai awal
+int nilaiDua[3] = {1, 2, 3};
+
