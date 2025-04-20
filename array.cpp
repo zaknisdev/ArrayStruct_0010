0e5adb616896ddbfa8f69ff473ba44b9f9a6dfce
zaknisdev <zakianisf1710@gmail.com>
2025-04-20 14:21:04 +0700

    membuat array dengan ukuran, tanpa nilai awal

--- array.cpp
+++ array.cpp
@@ -7,3 +7,5 @@ int nilai[] = {1, 2, 4, 7};
 // deklarasi array dengan ukuran dan diberi nilai awal
 int nilaiDua[3] = {1, 2, 3};
 
+//deklarasi array dengan ukuran , tanpa nilai awal
+string nama[4];
